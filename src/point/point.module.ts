33291c47bf4e70fa169d1f2063ab2c32dc1574e4
jeong <jw.jeong@tilda.co.kr>
2024-12-19 22:57:22 +0900

    lock 모듈 설정

--- src/point/point.module.ts
+++ src/point/point.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PointController } from './point.controller';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 import { PointService } from './point.service';
+import { LockModule } from '../lock/lock.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, LockModule],
   controllers: [PointController],
   providers: [PointService],
 })
